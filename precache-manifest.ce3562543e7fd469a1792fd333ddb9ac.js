93c587c77e148f40e4dcda45791059c7af6476b0
Göktürk Gök <gokturkgok@gmail.com>
2020-08-23 15:50:01 +0300

    Updates

---
+++ precache-manifest.ce3562543e7fd469a1792fd333ddb9ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071b977c80ca6af3c33b03a5022de96f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b55bc898e81fe94c466d",
+    "url": "/monsters-rolodex/static/css/main.d41dc021.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b55bc898e81fe94c466d",
+    "url": "/monsters-rolodex/static/js/main.271d717d.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
